c55091b611aee038ba4ee4bd999e874e6a5c9f15
NeyAlejandro <135058337+NeyAlejandro@users.noreply.github.com>
2023-12-11 10:48:17 -0500

    Update Enviardatos.py

--- Enviardatos.py
+++ Enviardatos.py
@@ -1,93 +1,72 @@
 import psutil
-import speedtest
-import paho.mqtt.publish as publish
+import json
+import paho.mqtt.client as mqtt
 import time
-import sqlite3
+import uuid
+from datetime import datetime
 
-# Configuración del servidor HiveMQ
-host = "broker.hivemq.com"  # Puedes cambiarlo si es necesario
-topic = "rendimiento_topic"
+mqtt_broker = "broker.hivemq.com"
+mqtt_port = 1883
+mqtt_topic = "Taller arquitectura"
 
-# Funciones para obtener el rendimiento del sistema
-def obtener_rendimiento_cpu():
-    return psutil.cpu_percent()
+def enviar_metricas(client):
+    cpu_percent = psutil.cpu_percent()
+    mem_percent = psutil.virtual_memory().percent
+    network_info = psutil.net_io_counters()
+    temperatura = obtener_temperatura()
 
-def obtener_rendimiento_memoria():
-    memory = psutil.virtual_memory()
-    memory_available = memory.available / (1024 * 1024)  # Convertir a MB
-    memory_used = memory.used / (1024 * 1024)  # Convertir a MB
-    return memory_available, memory_used
+    fecha_hora_actual = datetime.now().isoformat()
 
-def obtener_rendimiento_red():
+    datos_json = {
+        "cpu": cpu_percent,
+        "memory": mem_percent,
+        "net": {
+            "bytes_enviados": network_info.bytes_sent,
+            "bytes_recibidos": network_info.bytes_recv
+        },
+        "mac_address": obtener_mac_address(),
+        "temperature": temperatura,
+        "date": fecha_hora_actual
+    }
+
+    client.publish(mqtt_topic, json.dumps(datos_json))
+    print("Datos enviados:", datos_json)
+
+def obtener_temperatura():
     try:
-        st = speedtest.Speedtest()
-        st.get_best_server()  # Selecciona el mejor servidor automáticamente
-        download_speed = st.download() / 1e6  # Convertir a Mbps
-        upload_speed = st.upload() / 1e6  # Convertir a Mbps
-        return download_speed, upload_speed
+        with open("/sys/class/thermal/thermal_zone0/temp") as file:
+            temperatura_raw = float(file.read()) / 1000.0
+        return temperatura_raw
     except Exception as e:
-        print(f"Error al obtener datos de velocidad de red: {e}")
-        return None, None
+        return 0
 
-def crear_tabla(conexion):
-    cursor = conexion.cursor()
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS datos_rendimiento (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            cpu_percent REAL,
-            memory_available REAL,
-            memory_used REAL,
-            download_speed REAL,
-            upload_speed REAL,
-            timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
-        )
-    ''')
-    conexion.commit()
+def obtener_mac_address():
+    interfaz_activa = psutil.net_if_addrs().get('Ethernet', None)
 
-def insertar_datos(conexion, cpu_percent, memory_available, memory_used, download_speed, upload_speed):
-    cursor = conexion.cursor()
-    cursor.execute('''
-        INSERT INTO datos_rendimiento (
-            cpu_percent,
-            memory_available,
-            memory_used,
-            download_speed,
-            upload_speed
-        ) VALUES (?, ?, ?, ?, ?)
-    ''', (cpu_percent, memory_available, memory_used, download_speed, upload_speed))
-    conexion.commit()
+    if interfaz_activa is not None:
+        mac_address = interfaz_activa[0].address
+        return mac_address
+    else:
+        return None
 
-if __name__ == "__main__":
-    # Conectar a la base de datos SQLite (se crea si no existe)
-    conexion_db = sqlite3.connect('datos_rendimiento.db')
+def on_connect(client, userdata, flags, rc):
+    print(f"Conectado al broker con código de resultado: {rc}")
 
-    # Crear la tabla si no existe
-    crear_tabla(conexion_db)
+client = mqtt.Client()
+client.on_connect = on_connect
 
+client.connect(mqtt_broker, mqtt_port, 60)
+
+client.loop_start()
+
+try:
     while True:
-        try:
-            # Obtener el rendimiento del sistema
-            cpu_percent = obtener_rendimiento_cpu()
-            memory_available, memory_used = obtener_rendimiento_memoria()
-            download_speed, upload_speed = obtener_rendimiento_red()
+        enviar_metricas(client)
+        time.sleep(10)
+except KeyboardInterrupt:
+    pass
+finally:
+    client.loop_stop()
+    client.disconnect()
+    print("Conexión cerrada.")
 
-            # Insertar datos en la base de datos
-            insertar_datos(conexion_db, cpu_percent, memory_available, memory_used, download_speed, upload_speed)
-
-            # Crear un mensaje con los datos
-            mensaje = f"CPU: {cpu_percent}% | Memoria disponible: {memory_available:.2f} MB | Memoria en uso: {memory_used:.2f} MB"
-
-            if download_speed is not None and upload_speed is not None:
-                mensaje += f" | Descarga: {download_speed:.2f} Mbps | Subida: {upload_speed:.2f} Mbps"
-
-            # Publicar el mensaje en el tema MQTT
-            publish.single(topic, mensaje, hostname=host)
-
-        except Exception as e:
-            print(f"Error general: {e}")
-
-        # Esperar antes de recopilar nuevamente
-        time.sleep(60)  # Espera 60 segundos (puedes ajustar según tus necesidades)
-
-    # Cerrar la conexión a la base de datos al salir del bucle
-    conexion_db.close()
