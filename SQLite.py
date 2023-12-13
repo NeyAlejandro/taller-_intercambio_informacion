cceec2d90e5b437626dcae83f8b3409a7389c117
NeyAlejandro <135058337+NeyAlejandro@users.noreply.github.com>
2023-12-12 22:04:48 -0500

    Update SQLite.py

--- SQLite.py
+++ SQLite.py
@@ -1,111 +1,41 @@
 import psutil
-import wmi
-import sqlite3
-import time
+import platform
+import datetime
+import json
+import uuid
 
-def crear_tabla_uso_cpu(conexion):
-    cursor = conexion.cursor()
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS uso_cpu (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            porcentaje_cpu REAL,
-            porcentaje_memoria REAL,
-            interfaz_red TEXT,
-            bytes_enviados REAL,
-            bytes_recibidos REAL,
-            temperatura_cpu REAL,
-            timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
-        )
-    ''')
-    conexion.commit()
+def obtener_info_sistema():
+    info_sistema = {}
 
-def insertar_datos(conexion, porcentaje_cpu, porcentaje_memoria, interfaz, bytes_enviados, bytes_recibidos, temperatura_cpu):
-    cursor = conexion.cursor()
-    cursor.execute('''
-        INSERT INTO uso_cpu (
-            porcentaje_cpu,
-            porcentaje_memoria,
-            interfaz_red,
-            bytes_enviados,
-            bytes_recibidos,
-            temperatura_cpu
-        ) VALUES (?, ?, ?, ?, ?, ?)
-    ''', (porcentaje_cpu, porcentaje_memoria, interfaz, bytes_enviados, bytes_recibidos, temperatura_cpu))
-    conexion.commit()
+    info_sistema['cpu'] = psutil.cpu_percent(interval=1)
 
-def obtener_porcentaje_uso_cpu():
-    return psutil.cpu_percent(interval=1)
+    info_sistema['memoria'] = psutil.virtual_memory().percent
 
-def obtener_porcentaje_uso_memoria():
-    return psutil.virtual_memory().percent
+    info_sistema['red'] = {'bytes_enviados': psutil.net_io_counters().bytes_sent,
+                           'bytes_recibidos': psutil.net_io_counters().bytes_recv}
 
-def obtener_datos_red():
-    interfaces_red = psutil.net_io_counters(pernic=True)
-
-    primera_interfaz = next(iter(interfaces_red.keys()))
-    datos_interfaz = interfaces_red[primera_interfaz]
-
-    return primera_interfaz, datos_interfaz.bytes_sent, datos_interfaz.bytes_recv
-
-interfaz, bytes_enviados, bytes_recibidos = obtener_datos_red()
-
-def obtener_temperatura_cpu_windows():
     try:
-        conexion_wmi = wmi.WMI()
-        temperaturas_cpu = conexion_wmi.Win32_TemperatureProbe()
-
-        if not temperaturas_cpu:
-            print("No se pudo obtener información de temperatura del CPU.")
-            return None
-
-        # Tomar la primera temperatura disponible (puedes adaptar esto según tus necesidades)
-        primera_temperatura = next(iter(temperaturas_cpu))
-        return primera_temperatura.CurrentReading
-
+        info_sistema['temperatura'] = psutil.sensors_temperatures()
     except Exception as e:
-        print(f"Error al obtener la temperatura del CPU en Windows: {e}")
-        return None
+        info_sistema['temperatura'] = str(e)
 
-def obtener_datos_y_guardar():
-    # Conectar a la base de datos SQLite (se crea si no existe)
-    conexion = sqlite3.connect('datos_sistema.db')
+    info_sistema['mac'] = ':'.join(['{:02x}'.format((uuid.getnode() >> elements) & 0xff) for elements in range(5, -1, -1)])
 
-    # Crear la tabla si no existe
-    crear_tabla_uso_cpu(conexion)
+    info_sistema['fecha_hora'] = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-    # Obtener los datos
-    porcentaje_cpu = obtener_porcentaje_uso_cpu()
-    porcentaje_memoria = obtener_porcentaje_uso_memoria()
+    return info_sistema
 
-    interfaz, bytes_enviados, bytes_recibidos = obtener_datos_red()
+informacion = obtener_info_sistema()
 
-    temperatura_cpu = obtener_temperatura_cpu_windows()
+def json_formato():
 
-    # Insertar los datos en la base de datos
-    insertar_datos(conexion, porcentaje_cpu, porcentaje_memoria, interfaz, bytes_enviados, bytes_recibidos, temperatura_cpu)
+    json_info = json.dumps(informacion, indent=4)
 
-    # Cerrar la conexión a la base de datos
-    conexion.close()
+    print(json_info)
 
-def consultar_datos():
-    # Conectar a la base de datos SQLite
-    conexion = sqlite3.connect('datos_sistema.db')
+if __name__ == "__main__":
+    json_formato()
 
-    # Crear un cursor para ejecutar consultas
-    cursor = conexion.cursor()
-
-    # Ejecutar una consulta SQL para seleccionar todos los datos de la tabla uso_cpu
-    cursor.execute('SELECT * FROM uso_cpu')
-
-    # Obtener todos los resultados de la consulta
-    resultados = cursor.fetchall()
-
-    # Imprimir los resultados
-    for resultado in resultados:
-        print(resultado)
-
-    # Cerrar la conexión a la base de datos
-    conexion.close()
 
 if __name__ == "__main__":
     obtener_datos_y_guardar()
