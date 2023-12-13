af799ae9dc2d6e678b444c17aeb7ece0f7570837
NeyAlejandro <135058337+NeyAlejandro@users.noreply.github.com>
2023-12-12 22:11:01 -0500

    Update Enviardatos.py

--- Enviardatos.py
+++ Enviardatos.py
@@ -1,72 +1,36 @@
-import psutil
 import json
 import paho.mqtt.client as mqtt
-import time
-import uuid
 from datetime import datetime
 
-mqtt_broker = "broker.hivemq.com"
-mqtt_port = 1883
-mqtt_topic = "Taller arquitectura"
+class EnviadorMetricasMQTT:
+    def __init__(self, broker, port, topic, datos_rendimiento, base_datos):
+        self.client = mqtt.Client()
+        self.client.on_connect = self.on_connect
+        self.client.connect(broker, port, 60)
+        self.topic = topic
+        self.datos_rendimiento = datos_rendimiento
+        self.base_datos = base_datos
 
-def enviar_metricas(client):
-    cpu_percent = psutil.cpu_percent()
-    mem_percent = psutil.virtual_memory().percent
-    network_info = psutil.net_io_counters()
-    temperatura = obtener_temperatura()
+    def on_connect(self, client, userdata, flags, rc):
+        print(f"Conectado al broker con código de resultado: {rc}")
 
-    fecha_hora_actual = datetime.now().isoformat()
+    def enviar_metricas(self):
+        self.datos_rendimiento.actualizar()
+        datos_json = self.datos_rendimiento.to_json()
 
-    datos_json = {
-        "cpu": cpu_percent,
-        "memory": mem_percent,
-        "net": {
-            "bytes_enviados": network_info.bytes_sent,
-            "bytes_recibidos": network_info.bytes_recv
-        },
-        "mac_address": obtener_mac_address(),
-        "temperature": temperatura,
-        "date": fecha_hora_actual
-    }
+        # Insertar datos en la base de datos SQLite
+        datos_rendimiento = DatosRendimiento(self.datos_rendimiento)
+        self.base_datos.insertar_datos(datos_rendimiento)
 
-    client.publish(mqtt_topic, json.dumps(datos_json))
-    print("Datos enviados:", datos_json)
+        self.client.publish(self.topic, json.dumps(datos_json))
+        print("Datos enviados y guardados en la base de datos:", datos_json)
 
-def obtener_temperatura():
-    try:
-        with open("/sys/class/thermal/thermal_zone0/temp") as file:
-            temperatura_raw = float(file.read()) / 1000.0
-        return temperatura_raw
-    except Exception as e:
-        return 0
+    def iniciar_loop(self):
+        self.client.loop_start()
 
-def obtener_mac_address():
-    interfaz_activa = psutil.net_if_addrs().get('Ethernet', None)
-
-    if interfaz_activa is not None:
-        mac_address = interfaz_activa[0].address
-        return mac_address
-    else:
-        return None
-
-def on_connect(client, userdata, flags, rc):
-    print(f"Conectado al broker con código de resultado: {rc}")
-
-client = mqtt.Client()
-client.on_connect = on_connect
-
-client.connect(mqtt_broker, mqtt_port, 60)
-
-client.loop_start()
-
-try:
-    while True:
-        enviar_metricas(client)
-        time.sleep(10)
-except KeyboardInterrupt:
-    pass
-finally:
-    client.loop_stop()
+    def detener_loop(self):
+        self.client.loop_stop()
+        self.base_datos.cerrar_conexion()
     client.disconnect()
     print("Conexión cerrada.")
 
